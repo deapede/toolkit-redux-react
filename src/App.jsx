eee7f8b070705da4abfcf779bc52d3bbe7c7171d
David Pinto <pintodiaz.d@gmail.com>
2022-12-05 14:59:02 -0300

    Slices: increment, decrement, incrementByParam

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import reactLogo from './assets/react.svg';
+import { decrement, increment, incrementBy } from './store/slices/counter';
 import './App.css';
 
 function App() {
-    const [count, setCount] = useState(0);
+    const { counter } = useSelector((state) => state.counter);
+    const dispatch = useDispatch();
 
     return (
         <div className="App">
@@ -17,9 +19,12 @@ function App() {
                 </a>
             </div>
             <h1>Vite + React</h1>
+            <p>count is: {counter}</p>
             <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
+                <button onClick={() => dispatch(increment())}>Increment</button>
+                <button onClick={() => dispatch(decrement())}>Decrement</button>
+                <button onClick={() => dispatch(incrementBy(2))}>
+                    Increment By 2
                 </button>
             </div>
         </div>
